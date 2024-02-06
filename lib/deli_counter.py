74b2202591b570392ca25e3a113afa1e62c17f86
Tony Eder <tonyeder11@gmail.com>
2024-02-06 14:14:39 -0800

    gettin it slowly

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -2,38 +2,27 @@ katz_deli = []
 
 def line(katz_deli):
     peeps_in_line = []
-    greeting = 'The line is currently'
-    final_announcement = [ greeting ]
+    final_announcement = [ 'The line is currently:' ]
     index = 1
     if len(katz_deli) == 0:
         print("The line is currently empty.")
     elif len(katz_deli) >= 1:
         for peeps in katz_deli:
-            assignments = f'{index}. {peeps}.'
+            assignments = f'{index}. {peeps}'
             final_announcement.append(assignments)
             index += 1
         print(" ".join(final_announcement))
+
+def take_a_number(katz_deli, name):
+    index = 1
+    if len(katz_deli) == 0:
+        katz_deli.append(name)
+        print(f'Welcome, {name}. You are number {index} in line.')
+    elif len(katz_deli) >= 1:
+        katz_deli.append(name)
+        print(f'Welcome, {name}. You are number {len(katz_deli)} in line.')
             
 
-        # index = 1
-        # peeps_in_line = []
-        # greeting = ["The line is currently"]
-        # for peeps in katz_deli:
-            
-        #     spot_in_line = f"{str(index)}. {peeps}."
-        #     peeps_in_line.append(spot_in_line)
-        #     # new_greeting = " ".join(greeting)
-        #     breakpoint()
-        #     # print(new_greeting)
-        #     index += 1
-        #     # name_spot_join = ". ".join(name_spot)
-        #     # full_greeting = [ greeting, name_spot_join ]
-        #     # print(greeting)
-        #     # print(full_greet)
-
-
-def take_a_number():
-    return None
 
 def now_serving():
     return None
