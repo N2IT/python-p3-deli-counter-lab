169bff99df68d8889074b5059cdc46a68d3994aa
Tony Eder <tonyeder11@gmail.com>
2024-02-06 20:34:17 -0800

    getting desired outcome but not passing test

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -13,6 +13,7 @@ def line(katz_deli):
             index += 1
         print(" ".join(final_announcement))
 
+
 def take_a_number(katz_deli, name):
     index = 1
     if len(katz_deli) == 0:
@@ -23,6 +24,21 @@ def take_a_number(katz_deli, name):
         print(f'Welcome, {name}. You are number {len(katz_deli)} in line.')
             
 
+def now_serving(katz_deli):
+    # if there is nobody in line then it should print "There is nobody waiting to be served!"
+    if len(katz_deli) == 0:
+        print("There is nobody waiting to be served!")
+    # If there are folks in line it should call out 'Currently serving {name}' then they should be removed from the line
+    if len(katz_deli) > 0:
+        for i in range(len(katz_deli)):
+            print(f'Currently serving {katz_deli[0]}.')
+            katz_deli.pop(0)
 
-def now_serving():
-    return None
+
+# now_serving(['Tony', 'Manny', 'Xavi', 'Cathy'])
+
+        # index = len(katz_deli) - 1
+        # while index >= 0:
+        #     print(f'Currently serving {katz_deli[0]}.')
+        #     del katz_deli[0]
+        #     index -= 1
